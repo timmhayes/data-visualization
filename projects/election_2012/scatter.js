f516ec45bf34638c0ed59bf09a3107c9a493daee
Tim <tim@thetravelyear.com>
2014-01-30 21:08:18 -0500

    Formatting only

--- projects/election_2012/scatter.js
+++ projects/election_2012/scatter.js
@@ -20,95 +20,91 @@ d3.json("data.js", function (json) {
     }, false)
     loessWorker.postMessage({ "filters": filters, "json": json })
   }
-    /* ~~~~~~~~~~~~~~ establish sizing ~~~~~~~~~~~~~~ */
-    var data    = json.sort(function (a, b) { return a.pop > b.pop ? -1 : a.pop < b.pop ? 1 : 0 }),
-        padding = { top: 20, right: 130, bottom: 50, left: 100 },
-        size    = { height: 500, width: 900 },
 
-        format =  {
+  /* ~~~~~~~~~~~~~~ establish sizing ~~~~~~~~~~~~~~ */
+  var data = json.sort(function (a, b) { return a.pop > b.pop ? -1 : a.pop < b.pop ? 1 : 0 }),
+      padding = { top: 20, right: 130, bottom: 50, left: 100 },
+      size = { height: 500, width: 900 },
 
-          number: d3.format(","),
-          percent: d3.format(",p")
+      format = {
 
-        },
+        number: d3.format(","),
+        percent: d3.format(",p")
 
-        scales = {
+      },
 
-          y: d3.scale.linear()
-              .domain(d3.extent(data, function (d) { return d.result }).reverse())
-              .range([padding.top, size.height - padding.bottom]),
+      scales = {
 
-          radius: d3.scale.sqrt()// sqrt to set circle by area rather than radius  / r = sqrt(a/pi)
-              .domain(d3.extent(data, function (d) { return parseFloat(d.pop) }))
-              .range([0, 35]),
+        y: d3.scale.linear()
+            .domain(d3.extent(data, function (d) { return d.result }).reverse())
+            .range([padding.top, size.height - padding.bottom]),
 
-          ordinal: d3.scale.ordinal()
-              .domain(["Obama", "Tied", "Romney"])
-              .rangePoints([padding.top, size.height - padding.bottom])
+        radius: d3.scale.sqrt()// sqrt to set circle by area rather than radius  / r = sqrt(a/pi)
+            .domain(d3.extent(data, function (d) { return parseFloat(d.pop) }))
+            .range([0, 35]),
 
-        }
+        ordinal: d3.scale.ordinal()
+            .domain(["Obama", "Tied", "Romney"])
+            .rangePoints([padding.top, size.height - padding.bottom])
 
-    /* ~~~~~~~~~~~~~~ add interactive buttons ~~~~~~~~~~~~~~ */
+      }
 
-    var nav = d3.select(".chartwrapper").append("ul")
-    nav.attr("class", "nav")
-      .selectAll("li")
-      .data([
-        { title: "Racially Diverse", type: "min", tickFormat: "%,", description: "Percentage of minority (non-white) population" },
-        { title: "Wealthy", type: "inc", tickFormat: "$,", description: "Median household income of county" },
-        { title: "Educated", type: "grads", tickFormat: "%", description: "Percentage of people over 25 years old with a 4-year college degree or higher" },
-        { title: "On Welfare", type: "snap", tickFormat: "%,", description: "Percentage of households with cash public assistance or food stamps/SNAP in the last 12 months" },
-        { title: "Urban", type: "pop", tickFormat: ",", description: "Total population of county" }
-      ])
-      .enter()
-      .append("li")
-      .on("click", function (d) {
-        var clicked = this;
-        d3.selectAll(".nav li").classed("active", function () { return this == clicked })
-        draw(d)
-      })
-      .attr("title", function (d) { return d.description })
-      .text(function (d) { return d.title })
+  /* ~~~~~~~~~~~~~~ add interactive buttons ~~~~~~~~~~~~~~ */
 
-    nav.append("li")
-      .append("input")
-      .attr("placeholder", "search for county/state")
-      .on("keyup", function (d) { filter(this.value) })
+  var nav = d3.select(".chartwrapper").append("ul")
+  nav.attr("class", "nav")
+    .selectAll("li")
+    .data(filters)
+    .enter()
+    .append("li")
+    .on("click", function (d) {
+      var clicked = this;
+      d3.selectAll(".nav li").classed("active", function () { return this == clicked })
+      draw(d)
+    })
+    .attr("title", function (d) { return d.description })
+    .text(function (d) { return d.title })
 
-    /* ~~~~~~~~~~~~~~ intialize chart ~~~~~~~~~~~~~~ */
+  nav.append("li")
+    .append("input")
+    .attr("placeholder", "search for county/state")
+    .on("keyup", function (d) { filter(this.value) })
 
-    var chart = d3.select(".chartwrapper").append("svg")
-          .attr("class", "chart")
-          .attr("width", size.width)
-          .attr("height", size.height),
+  /* ~~~~~~~~~~~~~~ intialize chart ~~~~~~~~~~~~~~ */
 
-    /* ~~~~~~~~~~~~~~ axes and labels */
+  var chart = d3.select(".chartwrapper").append("svg")
+                .attr("class", "chart")
+                .attr("width", size.width)
+                .attr("height", size.height),
 
-    axisXElement = chart.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + (size.height - padding.bottom + 5) + ")"),
-    axisYElement = chart.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + (padding.left - 10) + ",0)"),
+  /* ~~~~~~~~~~~~~~ axes and labels */
 
-    labelX = axisXElement.append("text")
-      .attr("class", "labelX")
-      .attr("x", (size.width - padding.left - padding.right) / 2 + padding.left)
-      .attr("y", 35),
-    labelY = axisYElement.append("text")
-      .attr("class", "labelY")
-      .attr("transform", "rotate(-90)")
-      .attr("x", 0 - (size.height / 2) - 20)
-      .attr("y", -50)
-      .text("Margin of victory"),
+      axisXElement = chart.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + (size.height - padding.bottom + 5) + ")"),
+      axisYElement = chart.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + (padding.left - 10) + ",0)"),
 
-    dividerY = scales.y(0),
-    dividerLine = chart.append("line")
-      .attr("class", "divider")
-      .attr("x1", padding.left)
-      .attr("y1", dividerY)
-      .attr("x2", size.width - padding.right)
-      .attr("y2", dividerY)
+      labelX = axisXElement.append("text")
+        .attr("class", "labelX")
+        .attr("x", (size.width - padding.left - padding.right) / 2 + padding.left)
+        .attr("y", 35),
+      labelY = axisYElement.append("text")
+        .attr("class", "labelY")
+        .attr("transform", "rotate(-90)")
+        .attr("x", 0 - (size.height / 2) - 20)
+        .attr("y", -50)
+        .text("Margin of victory"),
+
+      dividerY = scales.y(0),
+      dividerLine = chart.append("line")
+        .attr("class", "divider")
+        .attr("x1", padding.left)
+        .attr("y1", dividerY)
+        .attr("x2", size.width - padding.right)
+        .attr("y2", dividerY)
+
     chart.append("text")
       .attr("x", size.width - padding.right)
       .attr("y", dividerY - 5)
@@ -122,46 +118,46 @@ d3.json("data.js", function (json) {
       .attr("class", "label-victor label-romney")
       .text("Romney Counties")
 
-    var tooltip = d3.select("body").append("div")
-      .attr("class", "tooltip")
+  var tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
 
-    /* ~~~~~~~~~~~~~~ population key */
+  /* ~~~~~~~~~~~~~~ population key */
 
-    var key  = chart.append("g")
-                .attr("class", "key")
-                .attr("transform", "translate(" + (size.width - padding.left) + "," + padding.top + ")"),
-        keys = key.selectAll("g")
-                .data([10000, 100000, 500000, 1000000])
-                .enter()
-                .append("g")
-                .attr("transform", function (d, i) { return "translate(" + 0 + "," + ((i + 1) * 25) + ")" })
+  var key = chart.append("g")
+              .attr("class", "key")
+              .attr("transform", "translate(" + (size.width - padding.left) + "," + padding.top + ")"),
+      keys = key.selectAll("g")
+              .data([10000, 100000, 500000, 1000000])
+              .enter()
+              .append("g")
+              .attr("transform", function (d, i) { return "translate(" + 0 + "," + ((i + 1) * 25) + ")" })
 
-    key.append("text")
-      .attr("class", "key-title")
-      .text("County Size")
-      .attr("y", 5)
+  key.append("text")
+    .attr("class", "key-title")
+    .text("County Size")
+    .attr("y", 5)
 
-    keys.append("circle")
-      .attr("r", function (d) { return scales.radius(d) })
+  keys.append("circle")
+    .attr("r", function (d) { return scales.radius(d) })
 
-    keys.append("text")
-      .attr("x", 20)
-      .attr("dy", "0.3em")
-      .text(function (d) { return format.number(d) })
+  keys.append("text")
+    .attr("x", 20)
+    .attr("dy", "0.3em")
+    .text(function (d) { return format.number(d) })
 
-    /* ~~~~~~~~~~~~~~ chart drawing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+  /* ~~~~~~~~~~~~~~ chart drawing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-    function draw(selectionData) {
+  function draw(filter) {
 
-      var dataColumn = selectionData.type
+    var dataColumn = filter.type
 
-      scales.x = d3.scale.linear()//pow().exponent(0.2)
-        .domain(d3.extent(data, function (d) { return parseFloat(d[dataColumn]) }))
-        .range([padding.left, size.width - padding.right])
+    scales.x = d3.scale.linear()//pow().exponent(0.4)
+                .domain(d3.extent(data, function (d) { return parseFloat(d[dataColumn]) }))
+                .range([padding.left, size.width - padding.right])
 
-      /* ~~~~~~~~~~~~~~ mouseover tooltip code */
+    /* ~~~~~~~~~~~~~~ mouseover tooltip code */
 
-      var mouseEvents =
+    var mouseEvents =
       {
         "mouseover": function (d) {
           d3.select(this)
@@ -220,14 +216,15 @@ d3.json("data.js", function (json) {
           return scales.y(d.result)
         })
 
-      /* ~~~~~~~~~~~~~~ change axis and labels */
+    /* ~~~~~~~~~~~~~~ change axis and labels */
 
-      var xAxis = d3.svg.axis().scale(scales.x).orient("bottom").tickFormat(d3.format(selectionData.tickFormat)),
+    var xAxis = d3.svg.axis().scale(scales.x).orient("bottom").tickFormat(d3.format(filter.tickFormat)),
           yAxis = d3.svg.axis().scale(scales.y).orient("left")
 
-      axisXElement.call(xAxis)
-      axisYElement.call(yAxis)
-      chart.selectAll(".labelX").text(selectionData.description)
+    axisXElement.call(xAxis)
+    axisYElement.call(yAxis)
+    chart.selectAll(".labelX").text(filter.description)
+
 
     /* ~~~~~~~~~~~~~~ Loess curve calculation */
 
@@ -256,20 +253,22 @@ d3.json("data.js", function (json) {
 
     }, 1000)
 
-    /* ~~~~~~~~~~~~~~ other helpers ~~~~~~~~~~~~~~ */
+  }
 
-    function filter(query) {
-      var clean = query.replace(/[^0-9a-zA-Z ÁÉÍÓÚ-áéíóu-ñÑ]+/, ""),
-          re = new RegExp(clean, "i")
-      chart.selectAll("svg>circle").attr("class", function (d) {
-        return re.test(d.name) ? "match" : "nomatch"
-      })
-    }
+  /* ~~~~~~~~~~~~~~ other helpers ~~~~~~~~~~~~~~ */
 
-    /* click first button in key onload */
+  function filter(query) {
+    var clean = query.replace(/[^0-9a-zA-Z ÁÉÍÓÚ-áéíóu-ñÑ]+/, ""),
+        re = new RegExp(clean, "i")
+    chart.selectAll("svg>circle").attr("class", function (d) {
+      return re.test(d.name) ? "match" : "nomatch"
+    })
+  }
 
-    var event = document.createEvent("HTMLEvents");
-    event.initEvent("click", true, false);
-    document.querySelector(".nav>li").dispatchEvent(event);
+  /* click first button in key onload */
+
+  var event = document.createEvent("HTMLEvents");
+  event.initEvent("click", true, false);
+  document.querySelector(".nav>li").dispatchEvent(event);
 
 })
